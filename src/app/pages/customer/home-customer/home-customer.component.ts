26e38de496bf2731601810b7d81fc7a991aa5498
Anıl Yılmaz <anilyilmaz108@gmail.com>
2024-08-27 01:12:13 +0300

    carousel-announcements

--- src/app/pages/customer/home-customer/home-customer.component.ts
+++ src/app/pages/customer/home-customer/home-customer.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CustomerStatsComponent } from './customer-stats/customer-stats/customer-stats.component';
 import { FindFlightComponent } from './find-flight/find-flight.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCarouselModule } from 'ng-zorro-antd/carousel';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
 
 @Component({
   selector: 'app-home-customer',
   standalone: true,
-  imports: [CustomerStatsComponent, FindFlightComponent],
+  imports: [CustomerStatsComponent, FindFlightComponent, NzCardModule, NzCarouselModule, NzSkeletonModule],
   templateUrl: './home-customer.component.html',
-  styleUrl: './home-customer.component.less'
+  styleUrl: './home-customer.component.less',
+  host: {ngSkipHydration: 'true'},
+
 })
 export class HomeCustomerComponent {
-
+  array = [1, 2, 3, 4];
 }
