6daac3534a866881465a920556b609893b64a570
Anıl Yılmaz <anilyilmaz108@gmail.com>
2024-09-04 14:48:47 +0300

    count-up directive and customer stats

--- src/app/helpers/index.ts
+++ src/app/helpers/index.ts
@@ -2,3 +2,5 @@ export * from './credit-card-number.pipe';
 export * from './generate-date';
 export * from './generate-id';
 export * from './generate-letter';
+export * from './count-up.directive';
+export * from './destroy';
--- src/app/pages/customer/home-customer/customer-stats/customer-stats/customer-stats.component.ts
+++ src/app/pages/customer/home-customer/customer-stats/customer-stats/customer-stats.component.ts
@@ -4,11 +4,12 @@ import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
 import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { CountUpDirective } from '../../../../../helpers/count-up.directive';
 
 @Component({
   selector: 'app-customer-stats',
   standalone: true,
-  imports: [NzCardModule,CommonModule, NzGridModule, NzStatisticModule, NzSkeletonModule],
+  imports: [NzCardModule,CommonModule, NzGridModule, NzStatisticModule, NzSkeletonModule, CountUpDirective],
   templateUrl: './customer-stats.component.html',
   styleUrl: './customer-stats.component.less'
 })
