ae76c23e282e1cca8be73159ac278d11047fef3c
Anıl Yılmaz <anilyilmaz108@gmail.com>
2024-09-06 11:57:18 +0300

    translate service for layout & footer

--- src/app/layouts/private-layout/footer/footer.component.ts
+++ src/app/layouts/private-layout/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { NzFooterComponent } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [NzFooterComponent, RouterLink],
+  imports: [NzFooterComponent, RouterLink, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.less'
 })
